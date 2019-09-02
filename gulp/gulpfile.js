3b73aa7b77b77a84d214c34205fb8fad15a399b3
lbjjade <2105931798@qq.com>
2019-09-02 10:49:57 +0800

    add gulp-preprocess

---
+++ gulp/gulpfile.js
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: libingjian
+ * @Date: 2019-08-29 14:31:58
+ * @LastEditors: libingjian
+ * @LastEditTime: 2019-08-29 14:32:09
+ */
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask
