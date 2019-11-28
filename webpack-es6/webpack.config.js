8f775d0044bd385b1b3888202d651155498001fb
lbjjade <2105931798@qq.com>
2019-11-28 21:22:22 +0800

    webpack-es6 init

---
+++ webpack-es6/webpack.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-28 21:11:51
+ * @LastEditTime: 2019-11-28 21:22:00
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \package-AMD-CMD-CommonJS\webpack-es6\webpack.config.js
+ */
+module.exports = {
+    entry: './main.js',
+    output: {
+      filename: 'bundle.js'
+    }
+  };
+  
